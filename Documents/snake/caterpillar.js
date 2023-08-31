91c6d5adc02a229e6c3940d97fc41ea1e3d9a9d8
Vincent Emery <141743943+VincentEmeryIII@users.noreply.github.com>
2023-08-31 17:31:08 -0400

    Update caterpillar.js

--- Documents/snake/caterpillar.js
+++ Documents/snake/caterpillar.js
@@ -1,24 +1,19 @@
-// Javascript code that controls the objects in the game
-// Created by Vincent Emery, James Searer, and Asani Bellamy
-// Must be used with index.html and caterpillar.css
-// COSC365 - Final - 5/3/23
-
 var blockSize = 25;
 var rows = 20;
 var columns= 20;
 var board;
 var context;
-var objectSize;
 
-//caterpillar
-var caterpillarX = blockSize * 5;
-var caterpillarY = blockSize * 5;
 
-//var color = "red";
+
+
+//Snake
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
 
 var velocityX = 0;
 var velocityY = 0;
-var caterpillarBody = [];
+var snakeBody = [];
 
 //Food
 var foodX;
@@ -27,6 +22,9 @@ var foodY;
 //score
 var currentScore = 0;
 var bestScore = 0;
+
+
+
 
 window.onload = function() {
     startGame();
@@ -39,23 +37,29 @@ window.onload = function() {
 function startGame(){
     
     board = document.getElementById("board")
-    board.height = 20 * blockSize;
-    board.width = 20 * blockSize;
+    board.height = 20 * 25;
+    board.width = 20 * 25;
     context = board.getContext("2d");
     randomFood();
 
-    caterpillarX = blockSize * 5;
-    caterpillarY = blockSize * 5;
+    snakeX = blockSize * 5;
+    
 
     velocityX = 0;
     velocityY = 0;
-    caterpillarBody = [];
+    snakeBody = [];
 
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
 }
 
+function playMusic() {
+    let play = document.getElementById("play");
+    let audio = new Audio("audio.mp3");
+    audio.play();
+}
+play.addEventListener(onclick, playMusic);
 //reset game when loss condition occurs
 function gameOver(){
     
@@ -69,6 +73,28 @@ function gameOver(){
     currentScore = 0;
     startGame();
     
+}
+
+//test currentScore with scores in DataBase to see if it is a high score - work in progress
+function testHighScore() {
+let play = document.getElementById("music");
+}
+
+//get name and post highscore to db - work in progress
+function newHighScore() {
+    var name = prompt("New High Score!!! Input your name","");
+    //need to change url - ask fries in class how ajax works
+    new Ajax.Request( "url",
+    {
+        method:"POST",
+        Parameters:{ name, currentScore},
+        onSuccess: startGame(),
+        onFailure: alert(failure),
+        onException: alert(exception)
+    }
+    );
+
+
 }
 
 //function to change type of food
@@ -96,7 +122,7 @@ function randomFood() {
     foodY = Math.floor(Math.random() * rows) * blockSize;
 }
 
-//change the direction of the caterpillar
+//change the direction of the snake
 function changeDirection(e) {
     if (e.code == "ArrowUp" && velocityY != 1) {
         velocityX = 0;
@@ -117,10 +143,12 @@ function changeDirection(e) {
     }
 }
 
+
+
 //updates game, checks for loss conditions
 function update() {
 
-    //keeps background black if fruit/caterpillar is not there
+    //keeps background black if fruit/snake is not there
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
@@ -131,39 +159,39 @@ function update() {
     //shows user their current score
     document.getElementById("currentScore").innerHTML = currentScore;
 
-    //test to see if caterpillar eats food
-    if(caterpillarX == foodX && caterpillarY == foodY) {
-        caterpillarBody.push([foodX, foodY]);
+    //test to see if snake eats food
+    if(snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
         randomFood();
         currentScore += 1;
     }
 
-    //creates caterpillar
-    for (let i = caterpillarBody.length - 1; i > 0; i--) {
-        caterpillarBody[i] = caterpillarBody[i-1];
+    //creates snake
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i-1];
     }
     
-    if (caterpillarBody.length) {
-        caterpillarBody[0]= [caterpillarX, caterpillarY];
+    if (snakeBody.length) {
+        snakeBody[0]= [snakeX, snakeY];
     }
 
     context.fillStyle = "lime";
-    caterpillarX += velocityX * blockSize;
-    caterpillarY += velocityY * blockSize;
-    context.fillRect(caterpillarX, caterpillarY, blockSize, blockSize);
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
 
-    for(let i = 0; i <caterpillarBody.length; i++) {
-        context.fillRect(caterpillarBody[i][0], caterpillarBody[i][1], blockSize, blockSize);
+    for(let i = 0; i <snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
     }
 
     //game over condition
-    if (caterpillarX < 0 || caterpillarX > columns*blockSize-1 || caterpillarY < 0 || caterpillarY > rows * blockSize-1) {
+    if (snakeX < 0 || snakeX > columns*blockSize-1 || snakeY < 0 || snakeY > rows * blockSize-1) {
         gameOver();
     }
 
     //game over condition
-    for (let i = 0; i < caterpillarBody.length; i++) {
-        if (caterpillarX == caterpillarBody[i][0] && caterpillarY == caterpillarBody[i][1]) {
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
             gameOver();
         }
     }
